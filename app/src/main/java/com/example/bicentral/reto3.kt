9206c25c9d0ea5b64521d1a1c330fb72910d0b25
Alex Portillo <alex.portillo@gmail.com>
2024-04-15 00:51:29 -0600

    Tercer reto 15/04/2024

---
+++ app/src/main/java/com/example/bicentral/reto3.kt
@@ -0,0 +1,62 @@
+package com.example.bicentral
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.api.local_api
+import com.example.bicentral.databinding.Reto2Binding
+import com.example.bicentral.databinding.Reto3Binding
+
+class reto3 : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: Reto3Binding
+    lateinit var user: EditText
+    lateinit var pass: EditText
+    var buttonAccept:Button? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = Reto3Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        user = binding.edTU
+        pass = binding.edTP
+        buttonAccept = binding.loginButton
+
+        buttonAccept?.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when(view!!.id){
+            R.id.loginButton->{
+                if(user.text.toString() != "" && pass.text.toString() != "")
+                    validaLogin(user.text.toString(), pass.text.toString())
+                else
+                    showToast("No contiene texto...")
+            }
+        }
+    }
+
+    private fun validaLogin(username: String, password: String){
+        val sessionToken = local_api().validateUser(username, password)
+
+        if(sessionToken != null){
+            val intent = Intent(this, reto3_menu::class.java).apply {
+                putExtra("SESSION_TOKEN", sessionToken)
+            }
+            startActivity(intent)
+        }else {
+            showToast("Credenciales incorrectas...")
+        }
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+}
---
+++ app/src/main/java/com/example/bicentral/reto3_menu.kt
@@ -0,0 +1,45 @@
+package com.example.bicentral
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.bicentral.databinding.Reto3MenuBinding
+
+class reto3_menu: AppCompatActivity() {
+    private lateinit var binding: Reto3MenuBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = Reto3MenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//setOnItemSelectedListener
+
+        replaceFragment(HomeFragment())
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener{
+            when(it.itemId){
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.shorts -> replaceFragment(ShortsFragment())
+                R.id.subscritions -> replaceFragment(SubscriptionFragment())
+                R.id.library -> replaceFragment(LibraryFragment())
+
+                else ->{
+
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
+
+    override fun onBackPressed() {
+        finishAffinity()
+    }
+}
